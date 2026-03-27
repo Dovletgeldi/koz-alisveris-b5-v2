bc61fe0b46733324254e31a273d6f07b987a8b27
Dovletgeldi Myradov <dovletm7@gmail.com>
2026-03-27 23:36:27 +0300

    feat: initial navbar structure and logo

--- koz-react/src/App.jsx
+++ koz-react/src/App.jsx
@@ -1,23 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <section id="center">
-        <h1 className="text-3xl font-bold underline text-blue-600">
-          Hello KöZ! Tailwind is working!
-        </h1>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <Navbar />
   )
 }
 
